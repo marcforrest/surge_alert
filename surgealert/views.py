bfa0c928a5be15095e03003e1dc648de3569fb00
marcforrest <marcforrest@gmail.com>
2015-03-27 15:35:41 +0200

    Update views.py

--- surgealert/views.py
+++ surgealert/views.py
@@ -7,25 +7,15 @@ from .models import Greeting
 # Create your views here.
 def index(request):
 	umhlanga = {'start_latitude':'-29.7275699','start_longitude':'31.0875162','end_latitude':'-29.7254724','end_longitude':'31.0711843'}
-	umhlangatop = {'start_latitude':'-29.7098408','start_longitude':'31.0857761','end_latitude':'-29.7326701','end_longitude':'31.0835223'}
 	durbanairport = {'start_latitude':'-29.623938','start_longitude':'31.097474','end_latitude':'-29.667131','end_longitude':'31.116346'}
-	durbannorth = {'start_latitude':'-29.784593','start_longitude':'31.038888','end_latitude':'-29.758671','end_longitude':'31.054951'}
 	stadium = {'start_latitude':'-29.826097','start_longitude':'31.029597','end_latitude':'-29.850944','end_longitude':'31.028358'}
-	icccentre = {'start_latitude':'-29.856081','start_longitude':'31.029860','end_latitude':'-29.870115','end_longitude':'31.046594'}
-	florida = {'start_latitude':'-29.834611','start_longitude':'31.017915','end_latitude':'-29.848767','end_longitude':'30.999229'}
 	
-	
-
 	cities = []
 	cities.append(umhlanga)
-	cities.append(umhlangatop)
 	cities.append(durbanairport)
-	cities.append(durbannorth)
 	cities.append(stadium)
-	cities.append(icccentre)
-	cities.append(florida)
 	info = ''
-	names = ["Umhlanga","Umhlanga Top","Durban Airport","Durban North","Stadium","ICC","Florida Road"]
+	names = ["Umhlanga","Durban Airport","Stadium"]
 	info = '<?xml version="1.0" encoding="UTF-8" ?> <rss version="2.0"> <channel> <title>Durban Surge</title><link></link>  <description></description>'
 	#info2
 	
